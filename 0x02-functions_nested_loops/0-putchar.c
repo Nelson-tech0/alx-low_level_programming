737350072288420058951b3585db01282d2447a1
Nelson.H <hilary.nelsonn@yahoo.com>
2022-07-05 08:48:24 -0700

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  * main - Entry point.
+ *    * Return: Always 0 (Success.)
+ *     **/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
