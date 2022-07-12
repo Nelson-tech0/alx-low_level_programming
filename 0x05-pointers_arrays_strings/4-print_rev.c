9d6e58ba2a54af3aec9b212a125dde3ae7d06da6
Nelson.H <hilary.nelsonn@yahoo.com>
2022-07-12 04:10:47 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,8 +9,10 @@
 void print_rev(char *s)
 {
 	int index;
+
 	/*finds the length of string without null character*/
 	for (index = 0; s[index] != '\0'; ++index)
+		;
 	/*print char from the last index as you decrement*/
 		for (--index; index >= 0; --index)
 			_putchar(s[index]);
