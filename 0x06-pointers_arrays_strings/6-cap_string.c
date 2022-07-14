2b47a0026ad88a69f43f0732bda66694e447e493
Nelson.H <hilary.nelsonn@yahoo.com>
2022-07-13 23:06:58 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,90 +1,40 @@
 #include "holberton.h"
-
 #include <stdio.h>
 
-
-
 /**
- *
  *  * isLower - Determines whether ascii is lowercase
- *
- *   *
- *
  *    * @c: Character
- *
- *     *
- *
  *      * Return: 1 if true, 0 if false
- *
- *       */
-
-
+ *       **/
 
 int isLower(char c)
-
 {
-
-		return (c >= 97 && c <= 122);
-
+	return (c >= 97 && c <= 122);
 }
 
-
-
 /**
- *
  *  * isDelimiter - Determines whether ascii is a delimiter
- *
- *   *
- *
  *    * @c: Character
- *
- *     *
- *
  *      * Return: 1 if true, 0 if false
- *
- *       */
-
-
+ *       **/
 
 int isDelimiter(char c)
-
 {
+	int i;
 
-		int i;
+	char delimiter[] = " \t\n,.!?\"(){}";
 
-			char delimiter[] = " \t\n,.!?\"(){}";
-
-
-
-				for (i = 0; i < 12; i++)
-
-							if (c == delimiter[i])
-
-											return (1);
-
-
-
-					return (0);
-
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
 }
 
-
-
 /**
- *
  *  * cap_string - Capitalizes all words of a string
- *
- *   *
- *
  *    * @s: Input string
- *
- *     *
- *
  *      * Return: string with capitalized words
- *
- *       */
-
-
+ *       **/
 
 char *cap_string(char *s)
 {
@@ -93,20 +43,16 @@ char *cap_string(char *s)
 
 	while (*s)
 	{
-		if (isDelimiter(*s)	
-				foundDelimit = 1;	
-				else if (isLower(*s) && foundDelimit)		
-				{						
-				*s -= 32;									foundDelimit = 0;
-				}
-													else
-
-																	foundDelimit = 0;
-
-															s++;
-
-																}
-
-					return (ptr);
-
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
+		s++;
+	}
+	return (ptr);
 }
