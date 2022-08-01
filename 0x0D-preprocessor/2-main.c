7fed9b1170e96d3e4684a7bc01909a52a24bd269
Nelson.H <hilary.nelsonn@yahoo.com>
2022-08-01 09:58:21 +0100

    task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints file name it was compiled from
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
