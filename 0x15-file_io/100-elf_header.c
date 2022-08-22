115888b234191f8894ae2a797546186f078b05c1
Nelson.H <hilary.nelsonn@yahoo.com>
2022-08-22 23:00:18 +0100

    0x15-files

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -91,93 +91,93 @@ void print_class(unsigned char *e_ident)
  * print_data - Prints the data of an ELF header.
  * @e_ident: A pointer to an array containing the ELF class.
  */
-OBOBOBOBOBOBOBOBOBvoid print_data(unsigned char *e_ident)
-OB{
-OBOB	printf("  Data:                              ");
-OBOB
-OBOB	switch (e_ident[EI_DATA])
-	{
-OBOB	case ELFDATANONE:
-OB		printf("none\n");
-OBOB		break;
-	case ELFDATA2LSB:
-OBOB		printf("2's complement, little endian\n");
-OB		break;
-	case ELFDATA2MSB:
-OBOBOB		printf("2's complement, big endian\n");
-OBOB		break;
-OB	default:
-		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-OBOBOB	}
-}
-OB
-/**
-OB * print_version - Prints the version of an ELF header.
- * @e_ident: A pointer to an array containing the ELF version.
-OBOBOBOB */
-void print_version(unsigned char *e_ident)
-OBOBOBOB{
-OB	printf("  Version:                           %d",
-OBOBOBOB	       e_ident[EI_VERSION]);
-
-OBOBOB	switch (e_ident[EI_VERSION])
-OB	{
-	case EV_CURRENT:
-OBOBOB		printf(" (current)\n");
-OBOB		break;
-OBOB	default:
-OB		printf("\n");
-OBOBOB		break;
-OB	}
-OBOB}
-OBOB
-OBOB/**
-OB * print_osabi - Prints the OS/ABI of an ELF header.
-OBOBOB * @e_ident: A pointer to an array containing the ELF version.
- */
-OBOBvoid print_osabi(unsigned char *e_ident)
+void print_data(unsigned char *e_ident)
 {
-OBOBOB	printf("  OS/ABI:                            ");
-OBOBOB
-	switch (e_ident[EI_OSABI])
-OBOBOB	{
-OBOB	case ELFOSABI_NONE:
-		printf("UNIX - System V\n");
-OB		break;
-	case ELFOSABI_HPUX:
-OBOBOB		printf("UNIX - HP-UX\n");
+	printf("  Data:                              ");
+
+	switch (e_ident[EI_DATA])
+	{
+	case ELFDATANONE:
+		printf("none\n");
 		break;
-OB	case ELFOSABI_NETBSD:
+	case ELFDATA2LSB:
+		printf("2's complement, little endian\n");
+		break;
+	case ELFDATA2MSB:
+		printf("2's complement, big endian\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+
+/**
+ * print_version - Prints the version of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF version.
+ */
+void print_version(unsigned char *e_ident)
+{
+	printf("  Version:                           %d",
+	       e_ident[EI_VERSION]);
+
+	switch (e_ident[EI_VERSION])
+	{
+	case EV_CURRENT:
+		printf(" (current)\n");
+		break;
+	default:
+		printf("\n");
+		break;
+	}
+}
+
+/**
+ * print_osabi - Prints the OS/ABI of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF version.
+ */
+void print_osabi(unsigned char *e_ident)
+{
+	printf("  OS/ABI:                            ");
+
+	switch (e_ident[EI_OSABI])
+	{
+	case ELFOSABI_NONE:
+		printf("UNIX - System V\n");
+		break;
+	case ELFOSABI_HPUX:
+		printf("UNIX - HP-UX\n");
+		break;
+	case ELFOSABI_NETBSD:
 		printf("UNIX - NetBSD\n");
 		break;
-OB	case ELFOSABI_LINUX:
+	case ELFOSABI_LINUX:
 		printf("UNIX - Linux\n");
-OB		break;
-	case ELFOSABI_SOLARIS:
-OBOB		printf("UNIX - Solaris\n");
 		break;
-OBOB	case ELFOSABI_IRIX:
+	case ELFOSABI_SOLARIS:
+		printf("UNIX - Solaris\n");
+		break;
+	case ELFOSABI_IRIX:
 		printf("UNIX - IRIX\n");
-OB		break;
+		break;
 	case ELFOSABI_FREEBSD:
-OB		printf("UNIX - FreeBSD\n");
-OB		break;
+		printf("UNIX - FreeBSD\n");
+		break;
 	case ELFOSABI_TRU64:
 		printf("UNIX - TRU64\n");
-OBOB		break;
+		break;
 	case ELFOSABI_ARM:
-OBOB		printf("ARM\n");
+		printf("ARM\n");
 		break;
 	case ELFOSABI_STANDALONE:
-OB		printf("Standalone App\n");
+		printf("Standalone App\n");
 		break;
-OB	default:
+	default:
 		printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 	}
-OB}
+}
 
 /**
-OBOB * print_abi - Prints the ABI version of an ELF header.
+ * print_abi - Prints the ABI version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF ABI version.
  */
 void print_abi(unsigned char *e_ident)
